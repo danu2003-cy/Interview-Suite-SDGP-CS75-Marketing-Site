f262e8e9152d2b060777ed263c9d16c26862d592
danu2003-cy <dhanuhshi.20230443@iit.ac.lk>
2025-02-23 16:38:41 +0530

    Configured GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Interview-Suite-SDGP-CS75-Marketing-Site",
+  base: "/Interview-Suite-SDGP-CS75-Marketing-Site/",
 })
